8ecf75e69fa3325ba05e2a4eb35b297340454526
Sean Narron <seannarron9@gmail.com>
2021-10-25 17:24:40 +0900

    Made it an SPA with router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import HomeComponent from './Components/HomeComponent';
+import WeatherComponent from './Components/WeatherComponent';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <HomeComponent />
+      <Router>
+        <>
+          <Switch>
+            <Route path="/weather/:id" exact component={WeatherComponent} />
+            <HomeComponent />
+          </Switch>
+        </>
+    </Router>
     </div>
   );
 }
